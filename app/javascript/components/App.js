5b368b1b2145c7ecb61b43df6bbc5096586eaac0
cnsaavedra <christian-saavedra@hotmail.com>
2019-05-18 17:16:06 -0400

    added react-router and dom

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -1,9 +1,19 @@
 import React from 'react'
+import { Route, Switch } from 'react-router-dom'
+import Home from './Home'
+import About from './About'
+import Tasks from './Tasks'
 
 class App extends React.Component {
     render () {
         return (
-            <div>Hello World</div>
+            <div>
+                <Switch>
+                    <Route exact path="/" component={Home} />
+                    <Route exact path="/about" component={About} />
+                    <Route exact path="/tasks" component={Tasks} />
+                </Switch>
+            </div>
         )
     }
 }
